975939426a8f699671fa5e13422176467fd18335
HMSufyanDev <hmsufyan.work@gmail.com>
2026-09-12 00:00:07 +0500

    exercise

--- 03-python-data-structures/exercises/advanced_dictionaries_data_processing.py
+++ 03-python-data-structures/exercises/advanced_dictionaries_data_processing.py
@@ -10,3 +10,16 @@ sorted_clients = list(sorted(
     key=lambda client: client["price"]
 ))
 
+
+
+# Word Frequency Counter
+text = "python data python lists data python"
+text_list = text.split()
+
+word_frequency = {}
+for word in text_list:
+    if word not in word_frequency:
+        word_frequency[word] = 0
+    word_frequency[word] += 1
+
+print(word_frequency)
